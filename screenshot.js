ccc181770ad24b41f46dac2f8730597477ed9766
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-06-06 04:30:07 +0530

    done

---
+++ screenshot.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+const screenShot = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.amazon.co.uk/", { waitUntil: "networkidle2" });
+    await page.screenshot({ path: "example.png" });
+  
+    await browser.close();
+  };
+  
+screenShot();
